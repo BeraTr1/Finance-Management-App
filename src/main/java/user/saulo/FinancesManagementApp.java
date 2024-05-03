ef83692a20b1b983fea508f88c8569145e2bc22a
BeraTr1 <beratr.alias@gmail.com>
2024-05-03 13:40:44 -0600

    Added loading and saving of data when the program starts and exits

--- src/main/java/user/saulo/FinancesManagementApp.java
+++ src/main/java/user/saulo/FinancesManagementApp.java
@@ -1,26 +1,39 @@
 package user.saulo;
 
 import user.saulo.application.App;
+import user.saulo.data.SQLite;
 import user.saulo.managers.AccountManager;
 import user.saulo.managers.AppManager;
+import user.saulo.managers.DataManager;
+
+import java.io.File;
 
 public class FinancesManagementApp {
+    public static FinancesManagementApp instance;
+
     public static AppManager appManager;
     public static AccountManager accountManager;
-    public static FinancesManagementApp instance;
+    public static DataManager dataManager;
 
     public static void main(String[] args) {
         FinancesManagementApp app = new FinancesManagementApp();
+        instance = app;
         app.start();
     }
 
     public void start() {
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
         this.loadManagers();
-        this.loadData();
 
-        // commands
-//        Commands commands = new Commands();
-//        commands.initialize();
+        try {
+            dataManager.loadData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         // front end
         App app = new App();
@@ -28,19 +41,17 @@ public class FinancesManagementApp {
     }
 
     public void end() {
-        this.saveData();
-    }
-
-    private void loadData() {
-
-    }
-
-    private void saveData() {
-
+        try {
+            dataManager.saveData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void loadManagers() {
         accountManager = new AccountManager();
         appManager = new AppManager();
+//        dataManager = new DataManager(new SQLite(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation() + File.separator + "database.db")));
+        dataManager = new DataManager(new SQLite(new File("").getAbsolutePath(), "database.db"));
     }
 }
