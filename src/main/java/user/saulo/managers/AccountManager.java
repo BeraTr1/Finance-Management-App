21eb9bca0a6ed42bebe1fb06a19c3d768a7f07d5
BeraTr1 <beratr.alias@gmail.com>
2024-05-02 23:56:32 -0600

    Added repay money function for when accounts need to repay their debt

--- src/main/java/user/saulo/managers/AccountManager.java
+++ src/main/java/user/saulo/managers/AccountManager.java
@@ -1,7 +1,10 @@
 package user.saulo.managers;
 
 import user.saulo.Account;
+import user.saulo.FinancesManagementApp;
+import user.saulo.MathUtils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,7 +12,6 @@ import java.util.Map;
 
 public class AccountManager {
     private final Map<String, Account> accounts = new HashMap<>();
-
     public Account createAccount(String accountName, String accountDescription) throws Exception {
         if (accounts.containsKey(accountName)) {
             throw new Exception("Account with name '" + accountName + "' already exists!");
@@ -53,52 +55,64 @@ public class AccountManager {
         addDebt(toAccount, amount);
     }
 
+    public void repayMoney(Account fromAccount, Account toAccount, double amount) throws Exception {
+        transferMoney(fromAccount, toAccount, amount);
+        removeCredit(toAccount, amount);
+        removeDebt(fromAccount, amount);
+    }
+
     public void addBalance(Account account, double amount) {
-        double newBalance = account.getBalance() + amount;
+        amount = MathUtils.roundDouble(amount);
+        double newBalance = MathUtils.roundDouble(new BigDecimal(Double.toString(account.getBalance())).add(new BigDecimal(Double.toString(amount))).doubleValue());
 
         account.setBalance(newBalance);
     }
 
     public void removeBalance(Account account, double amount) throws Exception {
+        amount = MathUtils.roundDouble(amount);
         double balance = account.getBalance();
 
         if (amount > balance) {
             throw new Exception(account.getName() + " does not have enough funds");
         }
 
-        double newBalance = balance - amount;
+        double newBalance = MathUtils.roundDouble(new BigDecimal(Double.toString(balance)).subtract(new BigDecimal(Double.toString(amount))).doubleValue());
         account.setBalance(newBalance);
     }
 
     public void addDebt(Account account, double amount) {
-        double newDebt = account.getDebt() + amount;
+        amount = MathUtils.roundDouble(amount);
+        double newDebt = MathUtils.roundDouble(new BigDecimal(Double.toString(account.getCredit())).add(new BigDecimal(Double.toString(amount))).doubleValue());
         account.setDebt(newDebt);
     }
 
     public void removeDebt(Account account, double amount) throws Exception {
+        amount = MathUtils.roundDouble(amount);
         double debt = account.getDebt();
 
         if (amount > debt) {
             throw new Exception(account.getName() + "'s debt is smaller than amount being removed ($" + amount + ")");
         }
 
-        double newDebt = debt - amount;
+        double newDebt = MathUtils.roundDouble(new BigDecimal(Double.toString(debt)).subtract(new BigDecimal(Double.toString(amount))).doubleValue());
         account.setDebt(newDebt);
     }
 
     public void addCredit(Account account, double amount) {
-        double newCredit = account.getCredit() + amount;
+        amount = MathUtils.roundDouble(amount);
+        double newCredit = MathUtils.roundDouble(new BigDecimal(Double.toString(account.getCredit())).add(new BigDecimal(Double.toString(amount))).doubleValue());
         account.setCredit(newCredit);
     }
 
     public void removeCredit(Account account, double amount) throws Exception {
+        amount = MathUtils.roundDouble(amount);
         double credit = account.getCredit();
 
         if (amount > credit) {
             throw new Exception(account.getName() + "'s credit is smaller than amount being removed ($" + amount + ")");
         }
 
-        double newCredit = credit - amount;
+        double newCredit = MathUtils.roundDouble(new BigDecimal(Double.toString(credit)).subtract(new BigDecimal(Double.toString(amount))).doubleValue());
         account.setCredit(newCredit);
     }
 }
